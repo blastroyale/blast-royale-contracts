a430881503af89798b89a787239f1bff3c379a81
Cryptobenkei <apuig75@gmail.com>
2022-03-30 08:10:14 +0200

    Add fees

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -5,154 +5,153 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 struct Listing {
   address owner;
-  bool is_active;
-  uint256 token_id;
+  bool isActive;
+  uint256 tokenId;
   uint256 price;
 }
 
 /// @title Blast Royale Token - $BLT
 /// @dev Based on OpenZeppelin Contracts.
-contract Marketplace is ReentrancyGuard  {
+contract Marketplace is ReentrancyGuard, Ownable  {
 
   using SafeMath for uint256;
 
-  uint256 public listing_count = 0;
+  uint256 public listingCount = 0;
+  uint256 public activeListingCount = 0;
+  uint256 private fee;
+  address private treasuryAddress;
+
   mapping (uint256 => Listing) public listings;
-  ERC721 private erc721_contract;
-  ERC20 private erc20_contract;
+  ERC721 private erc721Contract;
+  ERC20 private erc20Contract;
 
   /// @notice Event Listed 
-  event Listed(
-		uint listingId,
-		uint tokenId,
+  event ItemListed(
+		uint256 listingId,
+		uint256 tokenId,
 		address seller,
 		uint256 price
 	);
 
+  /// @notice Event Delisted 
+  event ItemDelisted(
+		uint256 listingId,
+		uint256 tokenId,
+		address seller
+	);
+
+  /// @notice EventItem Sold 
+  event ItemSold(
+		uint256 listingId,
+		uint256 tokenId,
+		address seller,
+		address buyer,
+    uint256 price
+	);
+
+  /// @notice Event Fee changed
+  event FeeChanged(
+    uint256 oldFee,
+    uint256 newFee,
+    address changedBy
+  );
+ 
   /// @notice Token constructor
   /// @dev Setup the two contracts it will interact with : ERC721 and ERC20
-  /// @param erc721_address Address of the NFT Contract.
-  /// @param erc20_address Address of the Primary Token Contract.
-  constructor(address erc721_address, address erc20_address) {
-    erc721_contract = ERC721(erc721_address);
-    erc20_contract = ERC20(erc20_address);
+  /// @param erc721Address Address of the NFT Contract.
+  /// @param erc20Address Address of the Primary Token Contract.
+  constructor(address erc721Address, address erc20Address) {
+    erc721Contract = ERC721(erc721Address);
+    erc20Contract = ERC20(erc20Address);
+    fee = 0;
   }
 
   /// @notice add a Listing to the Marketplace
   /// @dev Creates a new entry for a Listing object and transfers the Token to the contract
-  /// @param token_id NFT TokenId.
+  /// @param tokenId NFT TokenId.
   /// @param price Price in NFTs.
-  function addListing(uint256 token_id, uint256 price) public nonReentrant
+  function addListing(uint256 tokenId, uint256 price) public nonReentrant
   {
-    listings[listing_count] = Listing(
+    uint256 listingId = listingCount;
+    listings[listingId] = Listing(
       msg.sender,
       true,
-      token_id,
+      tokenId,
       price
     );
-    listing_count = listing_count.add(1);
-    erc721_contract.transferFrom(
+    listingCount = listingCount.add(1);
+    activeListingCount = activeListingCount.add(1);
+    erc721Contract.transferFrom(
       msg.sender,
       address(this),
-      token_id);
-    :
+      tokenId);
+
+    emit ItemListed(listingId, tokenId, msg.sender, price );   
   }
 
   /// @notice Remove a Listing from the Marketplace
   /// @dev Marks Listing as not active object and transfers the Token back
-  /// @param listing_id NFT Listing Id.
-  function removeListing(uint256 listing_id) public nonReentrant
+  /// @param listingId NFT Listing Id.
+  function removeListing(uint256 listingId) public nonReentrant
   {
-    require(listings[listing_id].owner == msg.sender, "Must be owner");
-    require(listings[listing_id].is_active, "Must be active");
-    listings[listing_id].is_active = false;
-    erc721_contract.transferFrom(
+    require(listings[listingId].owner == msg.sender, "Must be owner");
+    require(listings[listingId].isActive, "Must be active");
+    listings[listingId].isActive = false;
+    erc721Contract.transferFrom(
       address(this),
       msg.sender,
-      listings[listing_id].token_id
+      listings[listingId].tokenId
     );
+    activeListingCount = activeListingCount.sub(1);
+    emit ItemDelisted(listingId, listings[listingId].tokenId, msg.sender );   
   }
 
   /// @notice Buys a listed NFT
   /// @dev Trabsfers both the ERC20 token (price) and the NFT.
-  /// @param listing_id NFT Listing Id.
-  function buy(uint256 listing_id) public nonReentrant
+  /// @param listingId NFT Listing Id.
+  function buy(uint256 listingId) public nonReentrant
   {
-    require(listings[listing_id].is_active, "Must be active");
-    listings[listing_id].is_active = false;
-    erc20_contract.transferFrom(
+    require(listings[listingId].isActive, "Must be active");
+    listings[listingId].isActive = false;
+    uint256 buyingFee = (fee * listings[listingId].price / 10000);
+    erc20Contract.transferFrom(
       msg.sender,
-      listings[listing_id].owner,
-      listings[listing_id].price
+      listings[listingId].owner,
+      listings[listingId].price - fee
     );
-    erc721_contract.transferFrom(
+    if (fee > 0 ) {
+      erc20Contract.transferFrom(
+        msg.sender,
+       treasuryAddress,
+        fee
+      );
+    }
+    erc721Contract.transferFrom(
       address(this),
       msg.sender,
-      listings[listing_id].token_id
+      listings[listingId].tokenId
+    );
+    activeListingCount = activeListingCount.sub(1);
+    emit ItemSold(
+      listingId,
+      listings[listingId].tokenId,
+      listings[listingId].owner,
+      msg.sender,
+      listings[listingId].price
     );
   }
 
-  function getActiveListings(uint256 index) public view returns(uint256)
+  /// @notice Sets a new Fee
+  /// @param _fee new Fee.
+  /// @param _treasuryAddress New treasury address.
+  function setFee(uint256 _fee, address _treasuryAddress) public onlyOwner
   {
-    uint256 j;
-    for(uint256 i=0; i<listing_count; i++)
-    {
-      if(listings[i].is_active)
-      {
-        if(index == j)
-        {
-          return i;
-        }
-        j+=1;
-      }
-    }
-    return 0;
-  }
-
-  function getListingsByOwner(address owner, uint256 index) public view returns(uint)
-  {
-    uint256 j;
-    for(uint256 i=0; i<listing_count; i++)
-    {
-      if(listings[i].is_active && listings[i].owner == owner)
-      {
-        if(index == j)
-        {
-          return i;
-        }
-        j+=1;
-      }
-    }
-    return 0;
-  }
-
-  function getListingsByOwnerCount(address owner) public view returns(uint256)
-  {
-    uint256 result;
-    for(uint256 i=0; i<listing_count; i++)
-    {
-      if(listings[i].is_active && listings[i].owner == owner)
-      {
-        result+=1;
-      }
-    }
-    return result;
-  }
-
-  function getActiveListingsCount() public view returns(uint256)
-  {
-    uint256 result;
-    for(uint256 i=0; i<listing_count; i++)
-    {
-      if(listings[i].is_active)
-      {
-        result+=1;
-      }
-    }
-    return result;
+    fee = _fee;
+    treasuryAddress = _treasuryAddress;
   }
 }
 
--- test/marketplace.ts
+++ test/marketplace.ts
@@ -10,12 +10,14 @@ describe("Blast Royale Marketplace", function () {
   let admin: any;
   let player1: any;
   let player2: any;
+  let treasury: any;
 
   before("deploying", async () => {
     const signers = await ethers.getSigners();
     admin = signers[0];
     player1 = signers[1];
     player2 = signers[2];
+    treasury = signers[3];
   });
 
   it("Deploy Primary Token", async function () {
@@ -48,13 +50,78 @@ describe("Blast Royale Marketplace", function () {
 
   it("List an NFT to sell", async function () {
     await nft.connect(player1).approve(market.address, 0);
-    await market.connect(player1).addListing(0, ethers.utils.parseUnits("10"));
+    await expect(
+      market.connect(player1).addListing(0, ethers.utils.parseUnits("10"))
+    )
+      .to.emit(market, "ItemListed")
+      .withArgs(0, 0, player1.address, ethers.utils.parseUnits("10"));
+
+    const listingId = 0;
+    const totalListings = await market.activeListingCount();
+    expect(totalListings.toNumber()).to.equal(1);
+
+    // Get listing_id
+    const listing = await market.listings(listingId);
+    expect(listing.isActive).to.equal(true);
+    expect(listing.price).to.equal(ethers.utils.parseUnits("10"));
+    expect(listing.tokenId.toNumber()).to.equal(0);
+  });
+
+  it("Delist an NFT from the marketplace", async function () {
+    await expect(market.connect(player1).removeListing(0))
+      .to.emit(market, "ItemDelisted")
+      .withArgs(0, 0, player1.address);
   });
 
   it("Buy an NFT", async function () {
-    await blt
-      .connect(player2)
-      .approve(market.address, ethers.utils.parseUnits("10"));
-    // await market.connect(player2).buy(listingId);
+    // Add a new isting
+    await nft.connect(player1).approve(market.address, 0);
+    await market.connect(player1).addListing(0, ethers.utils.parseUnits("5"));
+    await nft.connect(player1).approve(market.address, 1);
+    await market.connect(player1).addListing(1, ethers.utils.parseUnits("10"));
+
+    // Get the total count of listings.
+    let totalListings = await market.activeListingCount();
+    expect(totalListings.toNumber()).to.equal(2);
+    const listingId = 2;
+    const listing = await market.listings(listingId);
+
+    // Approve BLT and But NFT from the marketplace.
+    await blt.connect(player2).approve(market.address, listing.price);
+    await expect(market.connect(player2).buy(listingId))
+      .to.emit(market, "ItemSold")
+      .withArgs(2, 1, player1.address, player2.address, listing.price);
+    totalListings = await market.activeListingCount();
+    expect(totalListings.toNumber()).to.equal(1);
+
+    // Check NFT was exchanged.
+    expect(await nft.ownerOf(1)).to.equal(player2.address);
+
+    // Check BLT was paid from player2 to player1
+    expect(await blt.balanceOf(player1.address)).to.equal(
+      ethers.utils.parseUnits("10")
+    );
+    expect(await blt.balanceOf(player2.address)).to.equal(
+      ethers.utils.parseUnits("90")
+    );
+  });
+
+  it("Add Fees", async function () {
+    await market.connect(admin).setFee(1000, treasury.address);
+    const listingId = 1;
+    const listing = await market.listings(listingId);
+    await blt.connect(player2).approve(market.address, listing.price);
+    await expect(market.connect(player2).buy(listingId))
+      .to.emit(market, "ItemSold")
+      .withArgs(1, 0, player1.address, player2.address, listing.price);
+    expect((await market.activeListingCount()).toNumber()).to.equal(0);
+
+    // Check BLT was paid from player2 to player1 and Fees were applied
+    expect(await blt.balanceOf(player1.address)).to.equal(
+      ethers.utils.parseUnits("10")
+    );
+    expect(await blt.balanceOf(player2.address)).to.equal(
+      ethers.utils.parseUnits("90")
+    );
   });
 });
