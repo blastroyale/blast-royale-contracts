34d54d77b80e48223d5becd889756eb91d4b0dfe
Jenson <Jenson.Collins@hotmail.com>
2022-04-05 18:12:25 +0900

    fix(scripts): Deployment and verify script & export address

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,7 +23,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   networks: {
     localhost: {
       url: "http://localhost:8545",
@@ -33,6 +41,11 @@ const config: HardhatUserConfig = {
     },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
---
+++ scripts/verify.ts
@@ -0,0 +1,47 @@
+import { BigNumber } from "ethers";
+import hre from "hardhat";
+import fs from "fs";
+
+async function main() {
+  const _address = fs.readFileSync("./scripts/address.json", {
+    encoding: "utf8",
+    flag: "r",
+  });
+  const addresses = JSON.parse(_address);
+
+  // Verify Marketplace Contract
+  await hre.run("verify:verify", {
+    address: addresses.marketplaceAddress,
+    constructorArguments: ["Blast Royale NFT", "BRW"],
+    contract: "contracts/BlastNFT.sol:BlastNFT",
+  });
+
+  // Verify Primary Token Contract
+  await hre.run("verify:verify", {
+    address: addresses.primaryToken,
+    constructorArguments: [
+      "Blast Token",
+      "BLT",
+      addresses.deployerAddress,
+      BigNumber.from("10000000000000000000000"),
+    ],
+    contract: "contracts/PrimaryToken.sol:PrimaryToken",
+  });
+
+  // Verify Secondary Token Contract
+  await hre.run("verify:verify", {
+    address: addresses.secondaryToken,
+    constructorArguments: [
+      "Craft Spice",
+      "CS",
+      addresses.deployerAddress,
+      BigNumber.from("10000000000000000000000"),
+    ],
+    contract: "contracts/SecondaryToken.sol:SecondaryToken",
+  });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
