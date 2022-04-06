2cb2e10b6f277bf04cccefeb45491795d79d7d4f
Jenson <Jenson.Collins@hotmail.com>
2022-04-06 22:26:54 +0900

    Wrong variable name it is not BlastToken

--- test/loot.ts
+++ test/loot.ts
@@ -6,16 +6,19 @@ import { ethers } from "hardhat";
 describe("Blast LootBox", function () {
   it("Test NFT", async function () {
     const [owner] = await ethers.getSigners();
-    const BlastToken = await ethers.getContractFactory("BlastLootBox");
-    const blt = await BlastToken.connect(owner).deploy("Blast LootBox", "BLB");
-    await blt.deployed();
+    const BlastLookBox = await ethers.getContractFactory("BlastLootBox");
+    const blb = await BlastLookBox.connect(owner).deploy(
+      "Blast LootBox",
+      "BLB"
+    );
+    await blb.deployed();
 
-    const tx = await blt
+    const tx = await blb
       .connect(owner)
       .safeMint(owner.address, "ipfs://111", [1, 2, 3]);
     await tx.wait();
 
-    expect(await blt.balanceOf(owner.address)).to.equal(1);
-    expect(await blt.tokenURI(0)).to.equal("ipfs://111");
+    expect(await blb.balanceOf(owner.address)).to.equal(1);
+    expect(await blb.tokenURI(0)).to.equal("ipfs://111");
   });
 });
