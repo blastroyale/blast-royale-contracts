bf92a9fb102fc2576a9e49d8de9f7d14198c78b1
corby0xa <106967482+0xcorby@users.noreply.github.com>
2022-06-23 18:03:34 +0100

    add functionality to generate merkle root
    - add helper function to generate merkle root
    - add jsons for WL addresses
    - change deployment script for lootboxMarketplace

--- scripts/deploy/helper.ts
+++ scripts/deploy/helper.ts
@@ -1,5 +1,7 @@
 import fs from "fs";
 import path from "path";
+import { ethers } from "hardhat";
+import { MerkleTree } from "merkletreejs";
 
 export const writeAddress = (params: any) => {
   const filePath = path.resolve(__dirname, "../address.json");
@@ -21,4 +23,58 @@ export const getAddress = () => {
     })
   );
   return addresses;
+};
+
+
+export const getMerkleRoots = async () => {
+const _whitelist = fs.readFileSync("./scripts/WL.json", {
+    encoding: "utf8",
+    flag: "r",
+  });
+  const whiltelist = JSON.parse(_whitelist);
+  if (!whiltelist) return;
+  
+  const _luckyWhitelist = fs.readFileSync("./scripts/luckyWL.json", {
+    encoding: "utf8",
+    flag: "r",
+  });
+  const luckyWhitelist = JSON.parse(_luckyWhitelist);
+  if (!luckyWhitelist) return;
+
+    const leaves = await Promise.all(
+        whiltelist.map(async (address:string) => {
+            return ethers.utils.keccak256(address);
+        })
+    ); 
+
+    const luckyLeaves = await Promise.all(
+        luckyWhitelist.map(async (address:string) => {
+            return ethers.utils.keccak256(address);
+        })
+    );
+    
+    const tree = new MerkleTree(leaves, ethers.utils.keccak256, {
+      sortPairs: true,
+    });
+    const merkleRoot = tree.getHexRoot();
+    console.log("merkleRoot: ",merkleRoot)
+
+    const luckyTree = new MerkleTree(luckyLeaves, ethers.utils.keccak256, {
+      sortPairs: true,
+    });
+    const luckyMerkleRoot = luckyTree.getHexRoot();
+    console.log("luckyMerkleRoot: ",luckyMerkleRoot)
+
+    return {merkleRoot,luckyMerkleRoot}
+};
+
+export const writeMerkleRoots = (params: any) => {
+  const filePath = path.resolve(__dirname, "../merkleRoots.json");
+  const merkleRoots = JSON.parse(
+    fs.readFileSync(filePath, {
+      encoding: "utf8",
+      flag: "r",
+    })
+  );
+  fs.writeFileSync(filePath, JSON.stringify(Object.assign(merkleRoots, params)));
 };
